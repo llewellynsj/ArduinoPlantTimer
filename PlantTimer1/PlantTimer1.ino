99222982213b5547f5f3906a391800f7a67fb63f
Llewellyn Sims Johns <llewellyn.sj@gmail.com>
2025-01-12 20:20:36 +1300

    Initial prototype version of code. Working so far: - Time for LED to light up, including sleep mode (timing needs to be tested over longer durations) - Push button to reset timer - Timer setting based on voltage divider - to be tested

--- PlantTimer1/PlantTimer1.ino
+++ PlantTimer1/PlantTimer1.ino
@@ -1,27 +1,61 @@
+/*
+ * File: PlantTimer1.ino
+ * Description: Prototype code for a personal project using an ATtiny85.
+ * This code is intended to control an LED and a button. The LED turns on after
+ * a certain period and stays on until the button is pressed. The device uses
+ * sleep modes to conserve power.
+ * 
+ * Microcontroller: ATtiny85
+ * Author: Llewellyn Sims Johns
+ * Date: 12 Jan 2025
+ */
+
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <avr/interrupt.h>
 
+#define LEDPIN 0 // Define LED pin as PB0 (digital pin 0)
+#define BUTTONPIN 4 // Define button pin as PB4 (digital pin 4)
+#define SWITCHPIN 3 // Define switch pin as PB3 (analog pin 3)
+
 volatile bool watchdog_wake = false;
 volatile unsigned long wake_count = 0;
+volatile bool button_pressed = false;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(BUTTONPIN, INPUT_PULLUP); // Set button pin as input with internal pull-up resistor
+  pinMode(SWITCHPIN, INPUT); // Set switch pin as input
+
+  // Enable pin change interrupt on BUTTONPIN
+  GIMSK |= (1 << PCIE); // Enable pin change interrupts
+  PCMSK |= (1 << BUTTONPIN); // Enable pin change interrupt for BUTTONPIN
 
   // Setup Watchdog Timer
   setupWatchdogTimer();
 }
 
 void loop() {
-  // If we've reached 10,800 wakeups (approx. 24 hours)
-  if (wake_count >= 5) {
-    // Perform your daily task
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
+  // If we've reached the wake_count threshold
+  if (wake_count >= getWakeCountThreshold()) {
+    // Turn on the LED
+    digitalWrite(LEDPIN, HIGH);
 
-    // Reset the counter
-    wake_count = 0;
+    // Wait for the button to be pressed
+    while (!button_pressed) {
+      // Enter idle sleep mode
+      set_sleep_mode(SLEEP_MODE_IDLE);
+      sleep_enable();
+      sleep_mode();
+      // The program will continue from here after waking up
+      sleep_disable();
+    }
+
+    // Turn off the LED
+    digitalWrite(LEDPIN, LOW);
+
+    // Reset the button press flag
+    button_pressed = false;
   }
 
   // Go to sleep
@@ -45,17 +79,38 @@ void goToSleep() {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_enable();
 
-  // Go to sleep, waiting for Watchdog Timer to wake us up
-  sleep_cpu();
+  // Go to sleep
+  sleep_mode();
 
-  // Waking up here, after sleep
-  sleep_disable();
+  // The program will continue from here after waking up
+  sleep_disable(); // First thing to do after waking up
 }
 
+// Watchdog Timer interrupt service routine
 ISR(WDT_vect) {
-  // This function is called when the Watchdog Timer wakes the MCU
   watchdog_wake = true;
-  wake_count++; // Increase the wake count
-  wdt_reset();  // Reset the Watchdog Timer
+  wake_count++;
 }
 
+// Pin change interrupt service routine
+ISR(PCINT0_vect) {
+  if (digitalRead(BUTTONPIN) == LOW) {
+    button_pressed = true;
+    wake_count = 0; // Reset the wake count
+  }
+}
+
+// Function to get the wake count threshold based on the switch position
+unsigned long getWakeCountThreshold() {
+  int switchValue = analogRead(SWITCHPIN);
+  if (switchValue < 341) { // Corresponds to 200K resistor
+    return 8 / 8; // 5 seconds (assuming watchdog timer interval is 8 seconds)
+  } else if (switchValue < 682) { // Corresponds to 1M resistor
+    return 60 / 8; // 1 minute
+  } else if (switchValue <= 1023) { // Corresponds to 5M resistor
+    return 600 / 8; // 10 minutes
+  } else {
+    // Default case if reading fails or is out of range
+    return 60 / 8; // Default to 1 minute
+  }
+}
